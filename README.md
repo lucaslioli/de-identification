642e79941381af51b7d45813048408f738e5763a
lucaslioli <loliveira@inf.ufsm.br>
2019-06-24 18:06:58 -0300

    Training and evaluating the model

--- README.md
+++ README.md
@@ -4,7 +4,11 @@ Experiments using CRF in Python for de-identification of clinical records. The d
 
 To replicate the experiments, download the clinical records from [MEDDOCAN datasets](http://temu.bsc.es/meddocan/index.php/data/) and paste it to the folder ```MEDDOCAN/```. Also, download the gazetteers from [MEDDOCAN resources](http://temu.bsc.es/meddocan/index.php/resources/) and paste it to the folder ```gazetteer/```, both inside the repository folder.
 
- To run the scripts it's needed to install the dependencies, described in file ```requirements.txt```.
+The initial experiments using CRF in Python were made based on Albert Au Yeung's article, "[Performing Sequence Labelling using CRF in Python](http://www.albertauyeung.com/post/python-sequence-labelling-with-crf/)".
+
+## Requirements
+
+To run the scripts it's needed to install the dependencies, described in file ```requirements.txt```.
 
     $ sudo pip install -r requirements.txt
 
@@ -14,5 +18,3 @@ After install all requirements it's also nedeed to download the following packag
     >>> import nltk
     >>> nltk.download('averaged_perceptron_tagger')
     >>> nltk.download('stopwords')
-
-The initial experiments using CRF in Python were made based on Albert Au Yeung's article, "[Performing Sequence Labelling using CRF in Python](http://www.albertauyeung.com/post/python-sequence-labelling-with-crf/)".
--- crf.py
+++ crf.py
@@ -1,17 +1,24 @@
-import glob                             # Read files from folder
-import string                           # Remove the ponctuation from the texts
-from pprint import pprint               # Data pretty printer
-import progressbar                      # Print progress bar
-from bs4 import BeautifulSoup as bs     # Parse XML files
-from nltk.corpus import stopwords       # Methods to remove stop words
-from nltk import pos_tag                # Methods generating Part-of-Speech tags
+import glob                                         # Read files from folder
+import string                                       # Remove the ponctuation from the texts
+import pycrfsuite                                   # Python implementation of Conditional Random Fields
+import progressbar                                  # Print progress bar
+import numpy as np                                  # Used in the evaluation process
+from pprint import pprint                           # Data pretty printer
+from bs4 import BeautifulSoup as bs                 # Parse XML files
+from nltk.corpus import stopwords                   # Methods to remove stop words
+from nltk import pos_tag                            # Methods generating Part-of-Speech tags
+from sklearn.metrics import classification_report   # Used to evaluate the results
 
-def prepare_data(path):
-    print("Preparing data for training...")
+TRAIN = "train"
+TEST = "test"
+
+# A function to prepare the data to fit with the need format
+def prepare_data(dataset):
+    print("Preparing data for {}ing...".format(dataset))
     progressbar.streams.wrap_stderr()
 
-    # Get all XML files from the path
-    files = glob.glob("{}/*.xml".format(path))
+    # Get all XML files from the specific dataset 
+    files = glob.glob("MEDDOCAN/{}-set/xml/*.xml".format(dataset))
 
     docs = []
 
@@ -57,6 +64,7 @@ def prepare_data(path):
         docs.append(texts)
     return docs
 
+# A function to preprocess and make a base clean of text
 def text_cleaner(text):
     # Getting the set of stop words
     stop = set(stopwords.words('spanish'))
@@ -74,8 +82,9 @@ def text_cleaner(text):
 
     return text
 
-def pos_tagging(docs):
-    print("Generating Part-of-Speech tags...")
+# A function to generate the Part-of-Speech tags for each document
+def pos_tagging(docs, dataset):
+    print("Generating Part-of-Speech tags for {}ing set...".format(dataset))
 
     data = []
     for doc in progressbar.progressbar(docs):
@@ -90,7 +99,137 @@ def pos_tagging(docs):
         data.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
     return data
 
+# A function generating the features
+def word2features(doc, i):
+    word = doc[i][0]
+    postag = doc[i][1]
+
+    # Common features for all words
+    features = [
+        'bias',
+        'word.lower=' + word.lower(),
+        'word[-3:]=' + word[-3:],
+        'word[-2:]=' + word[-2:],
+        'word.isupper=%s' % word.isupper(),
+        'word.istitle=%s' % word.istitle(),
+        'word.isdigit=%s' % word.isdigit(),
+        'postag=' + postag
+    ]
+
+    # Features for words that are not at the beginning of a document
+    if i > 0:
+        word1 = doc[i-1][0]
+        postag1 = doc[i-1][1]
+        features.extend([
+            '-1:word.lower=' + word1.lower(),
+            '-1:word.istitle=%s' % word1.istitle(),
+            '-1:word.isupper=%s' % word1.isupper(),
+            '-1:word.isdigit=%s' % word1.isdigit(),
+            '-1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'beginning of a document'
+        features.append('BOS')
+
+    # Features for words that are not at the end of a document
+    if i < len(doc)-1:
+        word1 = doc[i+1][0]
+        postag1 = doc[i+1][1]
+        features.extend([
+            '+1:word.lower=' + word1.lower(),
+            '+1:word.istitle=%s' % word1.istitle(),
+            '+1:word.isupper=%s' % word1.isupper(),
+            '+1:word.isdigit=%s' % word1.isdigit(),
+            '+1:postag=' + postag1
+        ])
+    else:
+        # Indicate that it is the 'end of a document'
+        features.append('EOS')
+
+    return features
+
+# A function for extracting features in documents
+def extract_features(doc):
+    return [word2features(doc, i) for i in range(len(doc))]
+
+# A function for generating the list of labels for each document
+def get_labels(doc):
+    return [label for (token, postag, label) in doc]
+
+# A function to group the process of extract features and get the labels
+def features_and_labels(train_data, test_data):
+    #TRAINING DATA
+    print("Extracting features from {}ing set...".format(TRAIN))
+    X_train = [extract_features(doc) for doc in progressbar.progressbar(train_data)]
+
+    print("Getting labels from {}ing set...".format(TRAIN))
+    y_train = [get_labels(doc) for doc in progressbar.progressbar(train_data)]
+
+    # TESTING DATA
+    print("Extracting features from {}ing set...".format(TEST))
+    X_test = [extract_features(doc) for doc in progressbar.progressbar(test_data)]
+
+    print("Getting labels from {}ing set...".format(TEST))
+    y_test = [get_labels(doc) for doc in progressbar.progressbar(test_data)]
+
+    return X_train, y_train, X_test, y_test
+
 if __name__ == '__main__':
-    docs = prepare_data("MEDDOCAN/train-set/xml")
-    data = pos_tagging(docs)
-    pprint(data[0][:10])
+    train_docs = prepare_data(TRAIN)
+    train_data = pos_tagging(train_docs, TRAIN)
+
+    test_docs = prepare_data(TEST)
+    test_data = pos_tagging(test_docs, TEST)
+
+    X_train, y_train, X_test, y_test = features_and_labels(train_data, test_data)
+
+    trainer = pycrfsuite.Trainer(verbose=False)
+
+    print("Training model...")
+
+    # Submit training data to the trainer
+    for xseq, yseq in zip(X_train, y_train):
+        trainer.append(xseq, yseq)
+
+    # Set the parameters of the model
+    trainer.set_params({
+        # coefficient for L1 penalty
+        'c1': 0.1,
+
+        # coefficient for L2 penalty
+        'c2': 0.01,  
+
+        # maximum number of iterations
+        'max_iterations': 200,
+
+        # whether to include transitions that
+        # are possible, but not observed
+        'feature.possible_transitions': True
+    })
+
+    # Provide a file name as a parameter to the train function, such that
+    # the model will be saved to the file when training is finished
+    trainer.train('crf.model')
+
+    tagger = pycrfsuite.Tagger()
+    tagger.open('crf.model')
+    y_pred = [tagger.tag(xseq) for xseq in X_test]
+
+    # # Let's take a look at a random sample in the testing set
+    # i = 12
+    # for x, y in zip(y_pred[i], [x[1].split("=")[1] for x in X_test[i]]):
+    #     print("%s (%s)" % (y, x))
+
+    print("Evaluating results...")
+
+    # Create a mapping of labels to indices
+    labels = {"PHI": 1, "NOT": 0}
+
+    # Convert the sequences of tags into a 1-dimensional array
+    predictions = np.array([labels[tag] for row in y_pred for tag in row])
+    truths = np.array([labels[tag] for row in y_test for tag in row])
+
+    # Print out the classification report
+    print(classification_report(
+        truths, predictions,
+        target_names=["NOT", "PHI"]))
