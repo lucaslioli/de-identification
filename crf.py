870eaf2a20f711f8a5423f25ef80022e90ae2b19
lucaslioli <loliveira@inf.ufsm.br>
2019-06-24 16:38:17 -0300

    Generating Part-of-Speech tags

--- crf.py
+++ crf.py
@@ -1,12 +1,13 @@
 import glob                             # Read files from folder
 import string                           # Remove the ponctuation from the texts
+from pprint import pprint               # Data pretty printer
 import progressbar                      # Print progress bar
 from bs4 import BeautifulSoup as bs     # Parse XML files
 from nltk.corpus import stopwords       # Methods to remove stop words
+from nltk import pos_tag                # Methods generating Part-of-Speech tags
 
 def prepare_data(path):
     print("Preparing data for training...")
-
     progressbar.streams.wrap_stderr()
 
     # Get all XML files from the path
@@ -58,7 +59,7 @@ def prepare_data(path):
 
 def text_cleaner(text):
     # Getting the set of stop words
-    stop = set(stopwords.words('portuguese'))
+    stop = set(stopwords.words('spanish'))
 
     # Getting the set of punctuation
     pont=set(string.punctuation)
@@ -73,5 +74,23 @@ def text_cleaner(text):
 
     return text
 
+def pos_tagging(docs):
+    print("Generating Part-of-Speech tags...")
+
+    data = []
+    for doc in progressbar.progressbar(docs):
+
+        # Obtain the list of tokens in the document
+        tokens = [t for t, label in doc]
+
+        # Perform POS tagging
+        tagged = pos_tag(tokens)
+
+        # Take the word, POS tag, and its label
+        data.append([(w, pos, label) for (w, label), (word, pos) in zip(doc, tagged)])
+    return data
+
 if __name__ == '__main__':
     docs = prepare_data("MEDDOCAN/train-set/xml")
+    data = pos_tagging(docs)
+    pprint(data[0][:10])
