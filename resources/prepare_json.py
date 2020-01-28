2e06b39b3505c54703b079cf35d58b8bf7809cc4
lucaslioli <loliveira@inf.ufsm.br>
2020-01-28 16:39:11 -0300

    Updated the data preparation script

--- resources/prepare_json.py
+++ resources/prepare_json.py
@@ -1,38 +1,62 @@
 import csv
 import json
 
-from statistics import mean
-
-FILEPATH = "dataset/DADOS_0.csv"
+FILEPATH = "dataset/DADOS_0-evolucoes"
 COLNAME = "EVOLUCOES"
 
 if __name__ == '__main__':
 
-    data = []
-    avg_size = []
-    avg_words = []
+    notes = []
 
-    with open(FILEPATH, mode="r", encoding="ISO-8859-1") as csvfile:
+    with open(FILEPATH+".csv", mode="r", encoding="ISO-8859-1") as csvfile:
         reader = csv.DictReader(csvfile)
 
         for row in reader:
             if row[COLNAME] is not None:
-                data.append({
+                notes.append({
                     'text': row[COLNAME],
                     'meta': {
-                        'size': len(row[COLNAME]),
+                        'code': row['CODIGO_PACIENTE'],
+                        'length': len(row[COLNAME]),
                         'words': len(row[COLNAME].split())
                     }
                 })
 
-                avg_size.append(len(row[COLNAME]))
-                avg_words.append(len(row[COLNAME].split()))
+        csvfile.close()
 
-    json_data = json.dumps(data, sort_keys=True, indent=4)
+    patients = {}
+    avg_length = 0
+    avg_words = 0
 
-    print(json_data)
+    for note in notes:
+        if note['meta']['code'] not in patients.keys():
+            patients[note['meta']['code']] = 0
+        patients[note['meta']['code']] += 1
 
-    # print("AVG Size: ", mean(avg_size))
-    # print("AVG Words: ", mean(avg_words))
+        del(note['meta'])
+
+        avg_length += len(note['text'])
+        avg_words += len(note['text'].split())
+
+    # print("Evolutions by patient:")
+    # print(json.dumps(patients, indent=2))
+
+    print("\nStatistics:\n")
+
+    print("Number of Evolutions: {}".format(len(notes)))
+    print("Number of Patients: {}".format(len(patients)))
+    print("Maximum evolution per patient: {}".format(max(patients.values())))
+
+    print("Average of Words: {}".format(int(avg_words/len(notes))))
+    print("Average length: {}".format(int(avg_length/len(notes))))
+
+    json_notes = json.dumps(notes, separators=('\n', ':'))
+
+    with open(FILEPATH+".json", mode='w', encoding="utf-8") as jsonfile:
+        jsonfile.write(json_notes[1:-1])
+
+        print("\nJSON file saved as: {}.json".format(FILEPATH))
+
+        jsonfile.close()
 
     exit()
